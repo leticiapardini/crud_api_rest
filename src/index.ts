5bdb993b0a03ceb359d03dc80227f9078103f447
Letícia Pardini <pardini448@gmail.com>
2022-11-15 09:01:57 -0300

    removendo bibliotecas

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import  passport  from 'passport';
-import { Auth } from './middlewares/auth';
+import  passport  from 'passport'
+import { Auth } from './middlewares/auth'
 import express from 'express'
 import { routes } from './routes/routes'
 import { AppDataSource } from './data-source'
 import flash from 'connect-flash'
 import session from 'express-session'
-
+import cors from 'cors'
 
 AppDataSource.initialize().then(() => {
   Auth(passport)
   const app = express()
+  app.use(cors())
   app.use(session({ secret: process.env.KEY as string | string[],
                     resave: true,
                     saveUninitialized: true}));
@@ -17,7 +18,7 @@ AppDataSource.initialize().then(() => {
   app.use(passport.session())
   app.use(express.json())
   app.use(flash());
-
+  
   app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
@@ -27,7 +28,7 @@ AppDataSource.initialize().then(() => {
 
   app.use(routes)
 
-  return app.listen(3000, () => {
+  return app.listen(8080, () => {
     console.log('running server!')
   })
 }).catch((err) => {
